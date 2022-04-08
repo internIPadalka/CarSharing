169e9b4497a84d5283594c2e9fee79a719528fd3
IvapPadalka <ipadalka@griddynamics.com>
2022-04-09 01:30:19 +0300

    Implemented stage 2

--- Main.java
+++ Main.java
@@ -1,51 +1,115 @@
 package carsharing;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    static final String JDBC_DRIVER = "org.h2.Driver";
-    static final String DB_URL = "jdbc:h2:~/Car Sharing/Car Sharing/task/src/carsharing/db/carsharing";
 
+    private final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
+        new Main().run();
+    }
+
+    public void run() {
+        int mainMenuChoice;
+        Connection connection;
+        Statement statement;
         try {
-            Class.forName(JDBC_DRIVER);
-
-
-            System.out.println("Connecting to database...");
-            conn = DriverManager.getConnection(DB_URL);
-            System.out.println("Creating table in given database...");
-            stmt = conn.createStatement();
-            String sql =  "CREATE TABLE   COMPANY " +
-                    "(id INTEGER not NULL, " +
-                    " NAME VARCHAR(255), " +
-                    " PRIMARY KEY ( id ))";
-            stmt.executeUpdate(sql);
-            System.out.println("Created table in given database...");
-            conn.setAutoCommit(true);
-            stmt.close();
-            conn.close();
-        } catch(SQLException se) {
-            se.printStackTrace();
-        } catch(Exception e) {
-            e.printStackTrace();
-        } finally {
-            try{
-                if(stmt!=null) stmt.close();
-            } catch(SQLException se2) {
-            }
+            connection = DriverManager.getConnection("jdbc:h2:~/Car Sharing/Car Sharing/task/src/carsharing/db/carsharing");
             try {
-                if(conn!=null) conn.close();
-            } catch(SQLException se){
-                se.printStackTrace();
+                statement = connection.createStatement();
+                statement.executeUpdate("CREATE TABLE IF NOT EXISTS COMPANY " +
+                        "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
+                        " name VARCHAR(255) UNIQUE NOT NULL " + ")");
+                statement.executeUpdate("ALTER TABLE COMPANY " + "ALTER COLUMN id RESTART WITH 1");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                while (true) {
+                    System.out.println("\n1. Log in as a manager \n0. Exit");
+                    mainMenuChoice = Integer.parseInt(scanner.nextLine());
+                    if (mainMenuChoice == 0) {
+                        break;
+                    } else if (mainMenuChoice == 1) {
+                        logInAsManager(connection);
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            connection.setAutoCommit(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void logInAsManager(Connection connection) throws SQLException {
+        int choice;
+        while (true) {
+            System.out.println("\n1. Company list \n2. Create a company \n0. Back");
+            choice = Integer.parseInt(scanner.nextLine());
+
+            if (choice == 0) {
+                break;
+            } else if (choice == 1) {
+                listAllCompanies(connection);
+            } else if (choice == 2) {
+                createACompany(connection);
             }
         }
-        System.out.println("Goodbye!");
+
+    }
+
+    private void createACompany(Connection connection) {
+        String companyName;
+        PreparedStatement preparedStatement;
+
+        System.out.println("Enter the company name:");
+        companyName = scanner.nextLine();
+
+        try {
+            preparedStatement = connection.prepareStatement("INSERT INTO COMPANY (name) " + "VALUES (?)");
+            preparedStatement.setString(1, companyName);
+            preparedStatement.executeUpdate();
+            System.out.println("The company was created!");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void listAllCompanies(Connection connection) {
+        Statement statement;
+        ResultSet companies;
+        List<String> companyList = new ArrayList<>();
+
+
+        try {
+            statement = connection.createStatement();
+            try {
+                companies = statement.executeQuery("SELECT * FROM COMPANY");
+                if (!companies.next()) {
+                    System.out.println("The company list is empty");
+                } else {
+                    while (true) {
+                        System.out.println(companies.getInt("id") + ". " + companies.getString("name"));
+                        companyList.add(companies.getString("name"));
+                        if (!companies.next()) {
+                            break;
+                        }
+                    }
+                    System.out.println("0. Back");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
