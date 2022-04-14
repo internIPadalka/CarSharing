34321497e564b47844890dc7af37384a49dbee3d
IvapPadalka <ipadalka@griddynamics.com>
2022-04-14 11:28:37 +0300

    Implemented stage 4

--- Main.java
+++ Main.java
@@ -21,46 +21,33 @@ public class Main {
             connection = DriverManager.getConnection("jdbc:h2:~/Car Sharing/Car Sharing/task/src/carsharing/db/carsharing");
             try {
                 statement = connection.createStatement();
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS COMPANY " +
-                        "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
-                        " name VARCHAR(255) UNIQUE NOT NULL " + ")");
+                statement.executeUpdate("CREATE TABLE IF NOT EXISTS COMPANY " + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " + " name VARCHAR(255) UNIQUE NOT NULL " + ")");
                 statement.executeUpdate("ALTER TABLE COMPANY " + "ALTER COLUMN id RESTART WITH 1");
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS CAR " +
-                        "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
-                        " name VARCHAR(255) UNIQUE NOT NULL, " +
-                        " company_id INT NOT NULL," +
-                        " CONSTRAINT fk_company FOREIGN KEY (company_id)" +
-                        " REFERENCES COMPANY(id)" +
-                        ")");
+                statement.executeUpdate("CREATE TABLE IF NOT EXISTS CAR " + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " + " name VARCHAR(255) UNIQUE NOT NULL, " + " company_id INT NOT NULL," + " CONSTRAINT fk_company FOREIGN KEY (company_id)" + " REFERENCES COMPANY(id)" + ")");
 
-                statement.executeUpdate("ALTER TABLE CAR " +
-                        "ALTER COLUMN id RESTART WITH 1");
+                statement.executeUpdate("ALTER TABLE CAR " + "ALTER COLUMN id RESTART WITH 1");
 
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS CUSTOMER " +
-                        "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
-                        " name VARCHAR(255) UNIQUE NOT NULL, " +
-                        " rented_car_id INT," +
-                        " CONSTRAINT fk_car FOREIGN KEY (rented_car_id)" +
-                        " REFERENCES CAR(id)" +
-                        ")");
+                statement.executeUpdate("CREATE TABLE IF NOT EXISTS CUSTOMER " + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " + " name VARCHAR(255) UNIQUE NOT NULL, " + " rented_car_id INT," + " CONSTRAINT fk_car FOREIGN KEY (rented_car_id)" + " REFERENCES CAR(id)" + ")");
 
-                statement.executeUpdate("ALTER TABLE CUSTOMER " +
-                        "ALTER COLUMN id RESTART WITH 1");
+                statement.executeUpdate("ALTER TABLE CUSTOMER " + "ALTER COLUMN id RESTART WITH 1");
 
-                statement.executeUpdate("ALTER TABLE CUSTOMER " +
-                        "ALTER rented_car_id SET DEFAULT NULL");
+                statement.executeUpdate("ALTER TABLE CUSTOMER " + "ALTER rented_car_id SET DEFAULT NULL");
             } catch (SQLException e) {
                 e.printStackTrace();
             }
 
             try {
                 while (true) {
-                    System.out.println("\n1. Log in as a manager \n0. Exit");
+                    System.out.println("\n1. Log in as a manager \n2. Log in as a customer \n3. Create a customer \n0. Exit");
                     mainMenuChoice = Integer.parseInt(scanner.nextLine());
                     if (mainMenuChoice == 0) {
                         break;
                     } else if (mainMenuChoice == 1) {
                         logInAsManager(connection);
+                    } else if (mainMenuChoice == 2) {
+                        logInAsCustomer(connection);
+                    } else if (mainMenuChoice == 3) {
+                        createACustomer(connection);
                     }
                 }
             } catch (SQLException e) {
@@ -165,8 +152,7 @@ public class Main {
         String carName;
         System.out.println("\nEnter the car name:");
         carName = scanner.nextLine();
-        String insertCompany = "INSERT INTO CAR (name, company_id) " +
-                "VALUES (?,?)";
+        String insertCompany = "INSERT INTO CAR (name, company_id) " + "VALUES (?,?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(insertCompany)) {
             preparedStatement.setString(1, carName);
             preparedStatement.setInt(2, chosenCompany);
@@ -178,42 +164,163 @@ public class Main {
         }
     }
 
-    private int listAllCar(Connection connection, int chosenCompany, String compName, String mOrC){
+    private int listAllCar(Connection connection, int chosenCompany, String compName, String mOrC) {
         Statement statement;
         ResultSet cars;
         int count = 1;
         try {
             statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             cars = statement.executeQuery("SELECT * FROM CAR WHERE company_id = '" + chosenCompany + "'");
-                if (!cars.next()) {
-                    if (mOrC.equals("manager")) {
-                        System.out.println("The car list is empty!");
-                        return -1;
-                    } else {
-                        System.out.println("No available cars in the '" + compName + "' company.");
-                        return -1;
-                    }
-
+            if (!cars.next()) {
+                if (mOrC.equals("manager")) {
+                    System.out.println("The car list is empty!");
+                    return -1;
                 } else {
-                    if (mOrC.equals("customer")) {
-                        System.out.println("\nChoose a car:");
-                    }
-                    while (true) {
-                        System.out.println(count + ". " + cars.getString("name") + ", car id: " + cars.getInt("id"));
-                        count++;
+                    System.out.println("No available cars in the '" + compName + "' company.");
+                    return -1;
+                }
 
-                        if (!cars.next()) {
-                            break;
-                        }
-                    }
-                    if (mOrC.equals("customer")) {
-                        cars.absolute(Integer.parseInt(scanner.nextLine()));
-                        return cars.getInt("id");
+            } else {
+                if (mOrC.equals("customer")) {
+                    System.out.println("\nChoose a car:");
+                }
+                while (true) {
+                    System.out.println(count + ". " + cars.getString("name") + ", car id: " + cars.getInt("id"));
+                    count++;
+
+                    if (!cars.next()) {
+                        break;
                     }
                 }
-            } catch (SQLException e){
+                if (mOrC.equals("customer")) {
+                    cars.absolute(Integer.parseInt(scanner.nextLine()));
+                    return cars.getInt("id");
+                }
+            }
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return 0;
+    }
+
+    private void createACustomer(Connection connection) {
+        PreparedStatement preparedStatement;
+        String customerName;
+        String insertCustomer;
+
+        System.out.println("Enter the customer name:");
+        customerName = scanner.nextLine();
+        insertCustomer = "INSERT INTO CUSTOMER (name) " + "VALUES (?)";
+        try {
+            preparedStatement = connection.prepareStatement(insertCustomer);
+            preparedStatement.setString(1, customerName);
+            preparedStatement.executeUpdate();
+            System.out.println("The customer was added!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void logInAsCustomer(Connection connection) throws SQLException {
+        Statement statement;
+        ResultSet customers;
+        int chosenCustomer = 0;
+
+        try {
+            statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            customers = statement.executeQuery("SELECT * FROM CUSTOMER");
+            if (!customers.next()) {
+                System.out.println("The customer list is empty!");
+            } else {
+                System.out.println("\nCustomer list:");
+                while (true) {
+                    System.out.println(customers.getInt("id") + ". " + customers.getString("name"));
+                    if (!customers.next()) {
+                        break;
+                    }
+                }
+                System.out.println("0. Back");
+                chosenCustomer = Integer.parseInt(scanner.nextLine());
+
+
+                if (chosenCustomer != 0) {
+                    customers.absolute(chosenCustomer);
+                    customerMenu(connection, chosenCustomer);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void customerMenu(Connection connection, int chosenCustomer) {
+        Statement statement;
+        boolean hasRentedCar = false;
+        int carId = -1;
+        ResultSet cus;
+        int choice = 0;
+        int neededCarId;
+        ResultSet car;
+        ResultSet rentedCar;
+        ResultSet belongToComp;
+        try {
+            statement = connection.createStatement();
+            while (true) {
+                cus = statement.executeQuery("SELECT * FROM CUSTOMER WHERE id = " + chosenCustomer);
+                cus.next();
+                carId = cus.getInt("rented_car_id");
+                if (carId != 0) {
+                    hasRentedCar = true;
+                }
+                System.out.println("\n1. Rent a car \n2. Return a rented car \n3. My rented car \n0. Back");
+                choice = Integer.parseInt(scanner.nextLine());
+                if (choice == 0) {
+                    break;
+                } else if (choice == 3) {
+                    if (!hasRentedCar) {
+                        System.out.println("You didn't rent a car!");
+                    } else {
+                        try {
+                            rentedCar = statement.executeQuery("SELECT * FROM CAR WHERE id = " + carId);
+                            System.out.println("You rented a car:");
+                            rentedCar.next();
+                            System.out.println(rentedCar.getString("name"));
+                            belongToComp = statement.executeQuery("SELECT * FROM COMPANY WHERE id = '" + rentedCar.getInt("company_id") + "'");
+                            System.out.println("Company:");
+                            belongToComp.next();
+                            System.out.println(belongToComp.getString("name"));
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                } else if (choice == 1) {
+                    if (hasRentedCar) {
+                        System.out.println("You've already rented a car!");
+                    } else {
+                        neededCarId = listAllCompanies(connection, "customer");
+                        if (neededCarId != 0) {
+                            statement.executeUpdate("UPDATE CUSTOMER SET rented_car_id = " + neededCarId + " WHERE id = " + chosenCustomer);
+                            try {
+                                car = statement.executeQuery("SELECT * FROM CAR WHERE id = " + neededCarId);
+                                car.next();
+                                System.out.println("You rented '" + car.getString("name") + "'");
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                } else if (choice == 2) {
+                    if (!hasRentedCar) {
+                        System.out.println("You didn't rent a car!");
+                    } else {
+                        statement.executeUpdate("UPDATE CUSTOMER SET rented_car_id = NULL " + "WHERE id = " + chosenCustomer);
+                        System.out.println("You've returned a rented car!");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
