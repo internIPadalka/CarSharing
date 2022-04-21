ae9c3a306a009345c7d7984e684e139edba9f5cd
IvapPadalka <ipadalka@griddynamics.com>
2022-04-22 01:23:25 +0300

    Fixed code

--- Main.java
+++ Main.java
@@ -9,31 +9,46 @@ public class Main {
 
     private final Scanner scanner = new Scanner(System.in);
 
+    private final String createTableIfNotExistsCompany = "CREATE TABLE IF NOT EXISTS COMPANY " + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " + " name VARCHAR(255) UNIQUE NOT NULL " + ")";
+    private final String alterTableCompanyWithID1 = "ALTER TABLE COMPANY " + "ALTER COLUMN id RESTART WITH 1";
+    private final String createTableIfNotExistsCar = "CREATE TABLE IF NOT EXISTS CAR " + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " + " name VARCHAR(255) UNIQUE NOT NULL, " + " company_id INT NOT NULL," + " CONSTRAINT fk_company FOREIGN KEY (company_id)" + " REFERENCES COMPANY(id)" + ")";
+    private final String alterTableCarWithID1 = "ALTER TABLE CAR " + "ALTER COLUMN id RESTART WITH 1";
+    private final String createTableIfNotExistsCustomer = "CREATE TABLE IF NOT EXISTS CUSTOMER " + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " + " name VARCHAR(255) UNIQUE NOT NULL, " + " rented_car_id INT," + " CONSTRAINT fk_car FOREIGN KEY (rented_car_id)" + " REFERENCES CAR(id)" + ")";
+    private final String alterTableCustomerWithID1 = "ALTER TABLE CUSTOMER " + "ALTER COLUMN id RESTART WITH 1";
+    private final String alterTableCustomerWithCarIDNULL = "ALTER TABLE CUSTOMER " + "ALTER rented_car_id SET DEFAULT NULL";
+
+    private final String selectAllFromTableCompanies = "SELECT * FROM COMPANY";
+    private final String selectAllFromTableCustomer = "SELECT * FROM CUSTOMER";
+
     public static void main(String[] args) {
         new Main().run();
     }
 
     public void run() {
+
         int mainMenuChoice;
-        Connection connection;
+
         Statement statement;
+
         try {
-            connection = DriverManager.getConnection("jdbc:h2:~/Car Sharing/Car Sharing/task/src/carsharing/db/carsharing");
+            Connection connection = DriverManager.getConnection("jdbc:h2:" + System.getProperty("user.dir") + "/Car Sharing/task/src/carsharing/db/carsharing");
             try {
                 statement = connection.createStatement();
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS COMPANY " + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " + " name VARCHAR(255) UNIQUE NOT NULL " + ")");
-                statement.executeUpdate("ALTER TABLE COMPANY " + "ALTER COLUMN id RESTART WITH 1");
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS CAR " + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " + " name VARCHAR(255) UNIQUE NOT NULL, " + " company_id INT NOT NULL," + " CONSTRAINT fk_company FOREIGN KEY (company_id)" + " REFERENCES COMPANY(id)" + ")");
+                statement.executeUpdate(createTableIfNotExistsCompany);
+                statement.executeUpdate(alterTableCompanyWithID1);
+                statement.executeUpdate(createTableIfNotExistsCar);
 
-                statement.executeUpdate("ALTER TABLE CAR " + "ALTER COLUMN id RESTART WITH 1");
+                statement.executeUpdate(alterTableCarWithID1);
 
-                statement.executeUpdate("CREATE TABLE IF NOT EXISTS CUSTOMER " + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, " + " name VARCHAR(255) UNIQUE NOT NULL, " + " rented_car_id INT," + " CONSTRAINT fk_car FOREIGN KEY (rented_car_id)" + " REFERENCES CAR(id)" + ")");
+                statement.executeUpdate(createTableIfNotExistsCustomer);
 
-                statement.executeUpdate("ALTER TABLE CUSTOMER " + "ALTER COLUMN id RESTART WITH 1");
+                statement.executeUpdate(alterTableCustomerWithID1);
 
-                statement.executeUpdate("ALTER TABLE CUSTOMER " + "ALTER rented_car_id SET DEFAULT NULL");
+                statement.executeUpdate(alterTableCustomerWithCarIDNULL);
+
             } catch (SQLException e) {
                 e.printStackTrace();
+                System.out.println("Exception after creation main table");
             }
 
             try {
@@ -52,15 +67,19 @@ public class Main {
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
+                System.out.println("Exception in choice menu");
             }
             connection.setAutoCommit(true);
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("Exception after establishing connection");
         }
     }
 
     private void logInAsManager(Connection connection) throws SQLException {
+
         int choice;
+
         while (true) {
             System.out.println("\n1. Company list \n2. Create a company \n0. Back");
             choice = Integer.parseInt(scanner.nextLine());
@@ -77,7 +96,9 @@ public class Main {
     }
 
     private void createACompany(Connection connection) {
+
         String companyName;
+
         PreparedStatement preparedStatement;
 
         System.out.println("Enter the company name:");
@@ -91,20 +112,28 @@ public class Main {
 
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("Exception after adding new company");
         }
     }
 
     private int listAllCompanies(Connection connection, String mOrC) {
+
         Statement statement;
+
         ResultSet companies;
+
         List<String> companyList = new ArrayList<>();
+
         int chosenCompany;
+        int statusCodeCompanyListIsEmpty = -1;
+        int statusCodeExceptionIsCached = 0;
+
         try {
             statement = connection.createStatement();
-            companies = statement.executeQuery("SELECT * FROM COMPANY");
+            companies = statement.executeQuery(selectAllFromTableCompanies);
             if (!companies.next()) {
                 System.out.println("The company list is empty");
-                return -1;
+                return statusCodeCompanyListIsEmpty;
             } else {
                 System.out.println("\nChoose the company:");
                 while (true) {
@@ -118,7 +147,6 @@ public class Main {
                 chosenCompany = Integer.parseInt(scanner.nextLine());
                 if (chosenCompany != 0) {
                     String compName = companyList.get(chosenCompany - 1);
-
                     if (mOrC.equals("manager")) {
                         carMenu(connection, chosenCompany, compName);
                     } else {
@@ -128,12 +156,15 @@ public class Main {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("Exception in listing companies");
         }
-        return 0;
+        return statusCodeExceptionIsCached;
     }
 
     private void carMenu(Connection connection, int chosenCompany, String compName) throws SQLException {
+
         int choiceCar;
+
         System.out.println("'" + compName + "' company");
         while (true) {
             System.out.println("\n1. Car list \n2. Create a car \n0. Back");
@@ -150,9 +181,12 @@ public class Main {
 
     private void createACar(Connection connection, int chosenCompany) {
         String carName;
+
         System.out.println("\nEnter the car name:");
         carName = scanner.nextLine();
+
         String insertCompany = "INSERT INTO CAR (name, company_id) " + "VALUES (?,?)";
+
         try (PreparedStatement preparedStatement = connection.prepareStatement(insertCompany)) {
             preparedStatement.setString(1, carName);
             preparedStatement.setInt(2, chosenCompany);
@@ -161,13 +195,18 @@ public class Main {
 
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("Exception in creating car");
         }
     }
 
     private int listAllCar(Connection connection, int chosenCompany, String compName, String mOrC) {
+
         Statement statement;
+
         ResultSet cars;
+
         int count = 1;
+
         try {
             statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             cars = statement.executeQuery("SELECT * FROM CAR WHERE company_id = '" + chosenCompany + "'");
@@ -199,12 +238,15 @@ public class Main {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("Exception in listing cars");
         }
         return 0;
     }
 
     private void createACustomer(Connection connection) {
+
         PreparedStatement preparedStatement;
+
         String customerName;
         String insertCustomer;
 
@@ -218,17 +260,21 @@ public class Main {
             System.out.println("The customer was added!");
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("Exception in adding new customer");
         }
     }
 
     private void logInAsCustomer(Connection connection) throws SQLException {
+
         Statement statement;
+
         ResultSet customers;
+
         int chosenCustomer = 0;
 
         try {
             statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            customers = statement.executeQuery("SELECT * FROM CUSTOMER");
+            customers = statement.executeQuery(selectAllFromTableCustomer);
             if (!customers.next()) {
                 System.out.println("The customer list is empty!");
             } else {
@@ -241,8 +287,6 @@ public class Main {
                 }
                 System.out.println("0. Back");
                 chosenCustomer = Integer.parseInt(scanner.nextLine());
-
-
                 if (chosenCustomer != 0) {
                     customers.absolute(chosenCustomer);
                     customerMenu(connection, chosenCustomer);
@@ -250,19 +294,25 @@ public class Main {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("Exception in login");
         }
     }
 
     private void customerMenu(Connection connection, int chosenCustomer) {
+
         Statement statement;
+
         boolean hasRentedCar = false;
+
         int carId = -1;
-        ResultSet cus;
         int choice = 0;
         int neededCarId;
+
+        ResultSet cus;
         ResultSet car;
         ResultSet rentedCar;
         ResultSet belongToComp;
+
         try {
             statement = connection.createStatement();
             while (true) {
@@ -291,6 +341,7 @@ public class Main {
                             System.out.println(belongToComp.getString("name"));
                         } catch (SQLException e) {
                             e.printStackTrace();
+                            System.out.println("Exception if you don't have car");
                         }
                     }
 
@@ -307,6 +358,7 @@ public class Main {
                                 System.out.println("You rented '" + car.getString("name") + "'");
                             } catch (SQLException e) {
                                 e.printStackTrace();
+                                System.out.println("Exception in choosing rented car");
                             }
                         }
                     }
@@ -321,6 +373,7 @@ public class Main {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("Exception in choosing a car");
         }
     }
 }
